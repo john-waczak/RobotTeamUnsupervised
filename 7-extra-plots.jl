9b99bc99d9b7074a3a46a9f88d8c4866b9cfb265
John Waczak <john.louis.waczak@gmail.com>
2024-05-14 20:39:20 -0500

    added plots for GTM diagram

--- 7-extra-plots.jl
+++ 7-extra-plots.jl
@@ -23,14 +23,81 @@ if !ispath(figures_path)
 end
 
 
-k = 32
-m = 14
-s = 0.1
+k = 15
+m = 5
+
+
 
 ξ₁ = range(-1, stop=1.0, length=k)
-ξ₁ = range(-1, stop=1.0, length=k)
+ξ₂ = range(-1, stop=1.0, length=k)
+
+μ₁ = range(-1, stop=1.0, length=m)
+μ₂ = range(-1, stop=1.0, length=m)
 
 
 
-# create figure with latent space grid and RBF points as well as
-# the "3d" projection of means in the "data" space
+s = 0.01
+function rbf_val(x,y)
+    out = 0.0
+    for i ∈ 1:m, j ∈ 1:m
+        out += exp(-((x-μ₁[i])^2 + (y - μ₂[j])^2)/(2*s))
+    end
+    out
+end
+
+cm = cgrad([CairoMakie.RGBA(0,0,0,0), mints_colors[2]]) #CairoMakie.RGBA(1,0,0, 1)])
+
+
+
+mints_colors[2]
+
+fig =Figure();
+ax = Axis(
+    fig[1,1],
+    xlabel="ξ₁", ylabel="ξ₂",
+    aspect=DataAspect(),
+    xticklabelsvisible=false, xticksvisible=false, xminorticksvisible=false, xgridvisible=false, xminorgridvisible=false, xlabelsize=30,
+    yticklabelsvisible=false, yticksvisible=false, yminorticksvisible=false, ygridvisible=false, yminorgridvisible=false, ylabelsize=30,
+);
+
+hidespines!(ax)
+
+pts_x = [(Point(ξ₁[i], -1), Point(ξ₁[i], 1)) for i ∈ 1:k]
+pts_y = [(Point(-1, ξ₂[i]), Point(1, ξ₂[i])) for i ∈ 1:k]
+pts_joined = [Point(ξ₁[i], ξ₂[j]) for i ∈ 1:k for j ∈ 1:k]
+
+pts_rbf = [Point(μ₁[i], μ₂[j]) for i ∈ 1:m for j ∈ 1:m]
+
+linesegments!(ax, pts_x, color=:lightgray, linewidth=2)
+linesegments!(ax, pts_y, color=:lightgray, linewidth=2)
+heatmap!(ax, -1:0.005:1, -1:0.005:1, rbf_val, colormap=cm)
+scatter!(ax, pts_joined, color=mints_colors[1], markersize=12)
+fig
+
+save(joinpath(figures_path, "gtm-latent.svg"), fig)
+
+
+
+# Create 3d plot of the grid with spheres on the node points
+
+fz(x,y) = x*exp(-x^2 -y^2)
+
+xs = range(-2, stop=2, length=k)
+ys = range(-2, stop=2, length=k)
+zs = [fz(xs[i], ys[j]) for i∈1:k, j∈1:k]
+
+pts = [Point3f(xs[i], ys[j], zs[i,j]) for i in 1:k for j ∈ 1:k]
+
+fig = Figure();
+ax = Axis3(fig[1,1]);
+hidedecorations!(ax)
+hidespines!(ax)
+
+
+#surface!(ax, xs, ys, fz, color=[CairoMakie.RGBA(0,0,0,0.5) for i ∈ 1:k for j ∈ 1:k]);
+wireframe!(ax, xs, ys, zs, color=:lightgray);
+scatter!(ax, pts, color=mints_colors[3], markersize=10)
+
+save(joinpath(figures_path, "gtm-projected.svg"), fig)
+
+fig
